e69b1be2c0e7dd72e38c8c5a01fcf9173c7c5b5d
Ronando Long <ronando.long@gmail.com>
2015-09-20 14:36:26 -0700

    Will resolve change greater than 5 censt and less than 10.

--- lib/coin_combinations.rb
+++ lib/coin_combinations.rb
@@ -1,9 +1,19 @@
 class Fixnum
   define_method(:coin_combinations) do
-    cents = self
+    total_cents = self
     change = [0, 0, 0, 0]
 
-    change[3] = cents
-    change
+    until total_cents == 0
+      if (total_cents > 10)
+        change[1] = (total_cents / 10)
+      elsif (total_cents > 5)
+        change[2] = (total_cents / 5)
+        total_cents = (total_cents % 5)
+      else
+        change[3] = total_cents
+        total_cents = 0
+      end
+    end
+    return change
   end
 end
